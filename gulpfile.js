f65068765749bfa370d1405fce3e923f8172361d
Darren Jaworski <djaworski@staplegun.us>
2015-10-04 19:04:36 -0500

    Gulp build.

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp'),
+  sass = require('gulp-sass'),
+  autoprefixer = require('gulp-autoprefixer'),
+  minify = require('gulp-minify-css'),
+  rename = require('gulp-rename');
+
+gulp.task('build-css', function() {
+
+  gulp.src('./style.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({
+      browsers: ['last 25 versions']
+    }))
+    .pipe(minify())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('./css'));
+
+});
+
+gulp.task('watch', function(){
+
+  gulp.watch('.**/*.scss', ['build-css']);
+
+});
+
+gulp.task('default', ['build-css']);
