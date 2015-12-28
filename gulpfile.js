18d98ff23aa3571d628e37b731d29744df93999e
Darren Jaworski <djaworski@staplegun.us>
2015-12-28 14:30:34 -0600

    Almost done integrating postcss.

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,31 @@
 var gulp = require('gulp'),
   sass = require('gulp-sass'),
-  autoprefixer = require('gulp-autoprefixer'),
-  minify = require('gulp-minify-css'),
   rename = require('gulp-rename'),
-  browserSync = require('browser-sync').create();
+  browserSync = require('browser-sync').create(),
+  postcss = require('gulp-postcss'),
+  autoprefixer = require('autoprefixer'),
+  cssnano = require('cssnano'),
+  colorRgbaFallback = require('postcss-color-rgba-fallback'),
+  opacity = require('postcss-opacity'),
+  pixrem = require('pixrem'),
+  mqpacker = require('css-mqpacker');
+
+var processors = [
+  autoprefixer({
+    browsers: ['last 25 versions']
+  }),
+  colorRgbaFallback,
+  opacity,
+  pixrem,
+  mqpacker,
+  cssnano
+];
 
 gulp.task('build-css', function() {
 
   gulp.src('style.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(autoprefixer({
-      browsers: ['last 25 versions']
-    }))
-    .pipe(minify())
+    .pipe(postcss(processors))
     .pipe(rename({suffix: '.min'}))
     .pipe(gulp.dest('./'))
     .pipe(browserSync.stream());
